0e117e6638991002f192ae1af000f02f30fd7112
Shevchenko Slava <kaganvmz@gmail.com>
2016-07-12 14:26:24 +0300

    added attr loop

--- inc/plugin_activation/init.php
+++ inc/plugin_activation/init.php
@@ -61,9 +61,8 @@ function qoob_register_required_plugins() {
 
         // Include SmartBuilder plugin.
         array(
-            'name' => 'qoob', // The plugin name.
-            'slug' => 'wp_qoob', // The plugin slug (typically the folder name).
-            'source' => 'http://qoob.it/build/wp_qoob_latest.zip', // The plugin source.
+            'name' => 'Qoob - Realtime Frontend Page Builder', // The plugin name.
+            'slug' => 'qoob', // The plugin slug (typically the folder name).
             'required' => true, // If false, the plugin is only 'recommended' instead of required.
             'version' => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
             'force_activation' => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
