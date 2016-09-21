e941dc07037e0428bf78cfea74b361e5c6263b8f
Shevchenko Slava <kaganvmz@gmail.com>
2016-09-21 14:10:22 +0300

    style: add page template docs, add style menu docs

--- functions.php
+++ functions.php
@@ -189,6 +189,16 @@ function qoob_theme_widgets_init() {
         'before_title' => '<h2 class="widget-title">',
         'after_title' => '</h2>',
     ));
+
+    register_sidebar(array(
+        'name' => esc_html__('Sidebar docs page', 'qoob'),
+        'id' => 'sidebar-docs',
+        'description' => esc_html__('Add widgets here.', 'qoob'),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h2 class="widget-title">',
+        'after_title' => '</h2>',
+    ));
 }
 
 add_action('widgets_init', 'qoob_theme_widgets_init');
---
+++ page-docs.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Page docs
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package qoob
+ */
+
+get_header(); ?>
+
+			<?php
+			$template_parts = 'page-docs';
+			while ( have_posts() ) : the_post();
+				$content = get_the_content();
+				
+				//Check for qoob page
+				$meta = get_post_meta(get_the_ID(), 'qoob_data', true);
+
+				if( (isset($_GET['qoob']) && $_GET['qoob'] == true) || ($meta != '{"blocks":[]}' && $meta != '') )
+					$template_parts = 'qoob';
+				
+				get_template_part( 'template-parts/content', $template_parts );
+
+			endwhile; // End of the loop.
+			?>
+			<?php
+?>
+<?php
+get_footer();
