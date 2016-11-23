a4ac274f549591b26a84874f9d0e595224e61ee8
portapro <uvarenkov@gmail.com>
2016-11-23 13:31:19 +0200

    style: 404 на webark.com/qoob #1213

--- 404.php
+++ 404.php
@@ -11,15 +11,26 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<section class="container content-404 relative">
-			    <div class="row">
-			        <div class="col-lg-12 main-block-404"></div>
-			    </div>
-			    <div class="row text-404">
-			        <h1><?php esc_html_e( 'Page not found', 'qoob' ); ?></h1>
-			    </div>
-			    <div class="row button-404">
-			        <a class="btn" href="/"><?php esc_html_e( 'Go to main page', 'qoob' ); ?></a>
+			<section class="container page-404 relative">			    
+			    <h1 class="header-block"><?php esc_html_e( 'Qoob - Realtime Frontend Page Builder for WordPress', 'qoob' ); ?></h1>
+			    
+			    <p><?php esc_html_e( 'It helps you build any template layouts for your website and features absolutely FREE', 'qoob' ); ?></p>
+			    <img alt="" src="<?php echo get_template_directory_uri(); ?>/images/qoob_404.png">
+
+			    <h3><?php esc_html_e( 'Sorry, currently no items are found. Please try again later.', 'qoob' ); ?></h3>
+
+			    <div class="col-container">
+			    	<div class="col-form">
+			    		<form role="search" method="get" class="searchform" action="/">
+							<input type="text" class="input-text" placeholder="<?php esc_html_e( 'Search', 'qoob' ); ?>" value="<?php the_search_query(); ?>" name="s" id="s" />
+							<button type="submit" class="button-submit">
+							    <i class="fa fa-search" aria-hidden="true"></i>
+							</button>
+						</form>
+			    	</div>
+			    	<div class="col-button">
+			    		<a class="btn" href="/"><?php esc_html_e( 'Go to main page', 'qoob' ); ?></a>
+			    	</div>
 			    </div>
 			</section><!-- end container-404 -->
 		</main><!-- #main -->
